d26d469c27851cfe9a07da3081c72699e36de26a
Vladislav <43294333+xoathx@users.noreply.github.com>
2023-08-18 22:08:37 +0300

    3
    3

--- LeanSoftTech/Program.cs
+++ LeanSoftTech/Program.cs
@@ -7,17 +7,16 @@ internal class Program
     private static void Main(string[] args)
     {
 
-		var filepath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);		
+		var filepath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);	
+		
 		PersonGenerate personGenerate = new PersonGenerate(10000); // Generate persons. 10.000 - amount
         var data = personGenerate.Persons;
 		SaveFile<List<Person>>.SaveAsJson(data, filepath);
 		personGenerate.Persons = null;
 		
 		ReadJson<Person> readJson = new ReadJson<Person>(filepath + @"\Persons.json");
-
 		PersonsConsole personsConsole = new PersonsConsole(readJson.JsonToList());
 		personsConsole.WriteInfo(); 
-
-		Console.WriteLine(); 
+ 
 	}
 }
